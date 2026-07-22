acbfe888e7df344b0dcb9209c597be057439a83b
Pallav Jagoori <pallav.jagoori@gmail.com>
2026-07-22 05:57:13 +0530

    fix(widget): bypass session middleware
    Keep the public widget shell usable in keyless CI and stateless deployments.

--- apps/api/routes/web.php
+++ apps/api/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\WidgetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/widget', [WidgetController::class, 'page']);
